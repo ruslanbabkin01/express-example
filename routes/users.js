5e719b8eb035d579ec576c213f3fade8d53c5e6f
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-06-25 17:59:29 +0300

    add users render

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const contacts = [
+
+const users = [
   { id: '1', username: 'Felix', surname: 'Brown', email: 'felix@test.com' },
   { id: '2', username: 'Sonya', surname: 'Redhead', email: 'sonya@test.com' },
   { id: '3', username: 'Conan', surname: 'Barbarian', email: 'conan@test.com' },
 ]
+
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.json(contacts)
+router.get('/', (req, res, next) => {
+  res.render('users', { users })
 })
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', (req, res, next) => {
   const { id } = req.params
-  const contact = contacts.filter(el => el.id === id)
-  res.json(contact)
+  const user = users.filter(el => el.id === id)
+  res.json(user)
 })
 
 module.exports = router
